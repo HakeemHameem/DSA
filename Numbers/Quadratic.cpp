c1df4ee95875c8c1e3e196df6c246e6a2dd93963
HakeemHameem <hameemhussain846@gmail.com>
2025-03-08 15:18:59 +0530

    second

--- Numbers/Quadratic.cpp
+++ Numbers/Quadratic.cpp
@@ -1,37 +1,37 @@
 #include<iostream>
 #include<math.h>
 using namespace std;
-
-int real(int a , int b , int c){
-    int det = (b * b) - 4 * a * c;
-    if(det > 0){
-        return det;
-    }
-    return 0;
-}
-
-
 int quad(int a , int b , int c){
-    
-    if(!real(a,b,c)){
-        return -1;
+   
+    int det  = (b * b) - 4 * a * c;
+    double r1 = (double) -b + sqrt((b * b) - 4 * a * c) / 2 * a ;
+    double r2 = (double) -b - sqrt((b * b) - 4 * a * c) / 2 * a ;
+    if(det < 0){
+        return -1; 
     }
-    else{
-        -b + sqtr(det)
+    if(det==0){
+        return 0;
     }
-    
-    
-
+        
+        else{
+            return 1;
+        }
 
 }
-
-
 
 int main(){
-    int a = 1 , b = -3 , c = -10;
+    int a = 1 , b = -6 , c = 9;
 
     int rat = quad(a,b,c);
     if(rat==-1){
         cout<<"Roots are complex as we got Iota"<<endl;
     }
+    else if(rat==0){
+        cout<<"Roots are Real and Equal"<<endl;
+    }
+    else{
+        cout<<"Roots are Real and Different"<<endl;
+    }
+
+    return 0;
 }
