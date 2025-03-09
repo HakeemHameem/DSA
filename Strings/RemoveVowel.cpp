baece24d0c8dd469d9315faabc46525f9bfc1405
HakeemHameem <hameemhussain846@gmail.com>
2025-03-09 12:28:42 +0530

    Remove Vowel

---
+++ Strings/RemoveVowel.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+    string str = "take u forward";
+    str.erase(remove_if(str.begin(), str.end(), [](char c) {
+        return string("aeiouAEIOU").find(c) != string::npos;
+    }), str.end());
+
+    cout << str;  
+    return 0;
+}
+
